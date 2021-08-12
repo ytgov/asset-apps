a2324a08104c93b7d95e058dfeeaa2991ff8cb3b
Max Parker <maxrparker@gmail.com>
2021-08-12 15:03:47 -0700

    Added loading circle. Would like to do foratting with classes though. And tlak about centering

--- web/src/views/NewScan.vue
+++ web/src/views/NewScan.vue
@@ -1,25 +1,37 @@
 <template>
   <div class="barcode">
-    <StreamBarcodeReader
-      @decode="onDecode"
-      @loaded="onLoaded"
-    ></StreamBarcodeReader>
-    <v-list subheader>
-      <v-subheader>Scanned Codes</v-subheader>
-      <v-list-item v-for="(entry, counter) in entries" :key="counter">
-        <v-list-item-content>
-          <v-list-item-title v-text="entry"></v-list-item-title>
-        </v-list-item-content>
-        <v-list-item-icon>
-          <v-icon @click="deleteEntry(counter)" style="float: right">
-            mdi-minus-circle
-          </v-icon>
-        </v-list-item-icon>
-      </v-list-item>
-    </v-list>
-    <v-btn color="success" class="mr-4" @click="activeSound"> Submit </v-btn>
+    <div>
+      <div v-if="loading">
+        <v-progress-circular
+          :size="50"
+          color="#f3b228"
+          indeterminate
+        ></v-progress-circular>
+      </div>
+      <StreamBarcodeReader
+        @decode="onDecode"
+        @loaded="onLoaded"
+        style="max-width: 640px"
+      ></StreamBarcodeReader>
+    </div>
+    <div v-if="!loading">
+      <v-list subheader>
+        <v-subheader>Scanned Codes</v-subheader>
+        <v-list-item v-for="(entry, counter) in entries" :key="counter">
+          <v-list-item-content>
+            <v-list-item-title v-text="entry"></v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-icon>
+            <v-icon @click="deleteEntry(counter)" style="float: right">
+              mdi-minus-circle
+            </v-icon>
+          </v-list-item-icon>
+        </v-list-item>
+      </v-list>
+      <v-btn color="success" class="mr-4" @click="activeSound"> Submit </v-btn>
 
-    <notifications ref="notifier"></notifications>
+      <notifications ref="notifier"></notifications>
+    </div>
   </div>
 </template>
 
@@ -37,6 +49,7 @@ export default {
     return {
       entries: [],
       id: null,
+      loading: true,
     };
   },
   setup() {
@@ -54,6 +67,7 @@ export default {
     },
     onLoaded() {
       console.log("load");
+      this.loading = false;
     },
     deleteEntry(counter) {
       this.entries.splice(counter, 1);
