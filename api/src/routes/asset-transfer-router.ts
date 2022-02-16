3769742f0c4581cbbee76443afdaad1e3f87cecf
Marlen Brunner <klondikemarlen@gmail.com>
2022-02-15 17:18:55 -0700

    :art: Prettify asset-transfer-routes.ts

--- api/src/routes/asset-transfer-router.ts
+++ api/src/routes/asset-transfer-router.ts
@@ -10,48 +10,64 @@ const PAGE_SIZE = 10;
 import { db } from "../data";
 const transferService = new TransferService(db);
 
-transferRouter.post("/", [body("page").isInt().default(1), body("itemsPerPage").isInt().default(10)],
+transferRouter.post(
+    "/",
+    [body("page").isInt().default(1), body("itemsPerPage").isInt().default(10)],
     async (req: Request, res: Response) => {
         let { query, sortBy, sortDesc, page, itemsPerPage } = req.body;
         let sort = new Array<SortStatement>();
 
         sortBy.forEach((s: string, i: number) => {
-            sort.push({ field: s, direction: sortDesc[i] ? SortDirection.ASCENDING : SortDirection.DESCENDING })
-        })
+            sort.push({
+                field: s,
+                direction: sortDesc[i]
+                    ? SortDirection.ASCENDING
+                    : SortDirection.DESCENDING,
+            });
+        });
 
         let skip = (page - 1) * itemsPerPage;
         let take = itemsPerPage;
 
-        let results = await transferService.doSearch(query, sort, page, itemsPerPage, skip, take);
+        let results = await transferService.doSearch(
+            query,
+            sort,
+            page,
+            itemsPerPage,
+            skip,
+            take
+        );
 
         return res.json(results);
+    }
+);
+
+transferRouter.post("/transfer", async (req: Request, res: Response) => {
+    let { from_owner_id, to_owner_id, rows, action } = req.body;
+
+    for (let row of rows) {
+        let transfer = {
+            asset_category_id: row.type,
+            request_user: req.user.email,
+            request_date: new Date(),
+            transfer_date: new Date(),
+            condition: row.condition,
+            from_owner_id,
+            to_owner_id,
+            quantity: row.quantity,
+            description: row.tag,
+        };
+
+        await db("asset_transfer").insert(transfer);
+    }
+
+    return res.json({
+        messages: { variant: "success", text: "Transfer saved" },
     });
+});
 
-transferRouter.post("/transfer",
-    async (req: Request, res: Response) => {
-        let { from_owner_id, to_owner_id, rows, action } = req.body;
-
-        for (let row of rows) {
-            let transfer = {
-                asset_category_id: row.type,
-                request_user: req.user.email,
-                request_date: new Date(),
-                transfer_date: new Date(),
-                condition: row.condition,
-                from_owner_id,
-                to_owner_id,
-                quantity: row.quantity,
-                description: row.tag
-            }
-
-            await db("asset_transfer").insert(transfer);
-        }
-
-        return res.json({ messages: { variant: "success", text: "Transfer saved" } });
-    });
-
-
-transferRouter.post("/transfer-request",
+transferRouter.post(
+    "/transfer-request",
     async (req: Request, res: Response) => {
         let { asset, mailcode, rows, condition } = req.body;
 
@@ -64,12 +80,11 @@ transferRouter.post("/transfer-request",
                 condition: `REQUEST: ${condition}`,
                 from_owner_id: mailcode,
                 to_owner_id: 80,
-                quantity: 1
+                quantity: 1,
             };
 
             await db("asset_transfer").insert(transfer);
-        }
-        else {
+        } else {
             for (let row of rows) {
                 let transfer = {
                     asset_category_id: row.type,
@@ -79,19 +94,21 @@ transferRouter.post("/transfer-request",
                     condition: `REQUEST: ${row.condition}`,
                     from_owner_id: mailcode,
                     to_owner_id: 80,
-                    quantity: row.quantity
+                    quantity: row.quantity,
                 };
 
                 await db("asset_transfer").insert(transfer);
             }
         }
 
-        return res.json({ messages: { variant: "success", text: "Transfer saved" } });
-    });
-
+        return res.json({
+            messages: { variant: "success", text: "Transfer saved" },
+        });
+    }
+);
 
 transferRouter.delete("/:id", async (req: Request, res: Response) => {
-    let { id } = req.params;
+    const { id } = req.params;
 
     await db("asset_transfer").where({ id }).delete();
     return res.json({
@@ -100,11 +117,13 @@ transferRouter.delete("/:id", async (req: Request, res: Response) => {
     });
 });
 
-transferRouter.get("/clean",
-    async (req: Request, res: Response) => {
-        let { id } = req.params;
+transferRouter.get("/clean", async (req: Request, res: Response) => {
+    let { id } = req.params;
 
-        await transferService.clean();
+    await transferService.clean();
 
-        return res.json({ data: {}, messages: [{ variant: "success", text: "Location removed" }] });
+    return res.json({
+        data: {},
+        messages: [{ variant: "success", text: "Location removed" }],
     });
+});
