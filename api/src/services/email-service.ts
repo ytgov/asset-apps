7c3bd6bfd4524c3e5cfb10de764b1ed3aa1eff9a
Marlen Brunner <klondikemarlen@gmail.com>
2022-03-08 10:14:18 -0700

    :art: Lint/prettify email-service.ts and config.ts

--- api/src/services/email-service.ts
+++ api/src/services/email-service.ts
@@ -1,9 +1,17 @@
-import nodemailer, { Transporter, TransportOptions } from "nodemailer";
-import { MailOptions } from "nodemailer/lib/json-transport";
-import { MAIL_CONFIG, MAIL_FROM, NODE_ENV, FRONTEND_URL, APPLICATION_NAME, MAIL_CONFIG_DEV } from "../config";
 import fs from "fs";
 import path from "path";
 
+import nodemailer, { Transporter, TransportOptions } from "nodemailer";
+import { MailOptions } from "nodemailer/lib/json-transport";
+
+import {
+    MAIL_CONFIG,
+    MAIL_FROM,
+    NODE_ENV,
+    FRONTEND_URL,
+    APPLICATION_NAME,
+    MAIL_CONFIG_DEV,
+} from "../config";
 import { AuthUser } from "../data";
 const BASE_TEMPLATE = "../templates/base.html";
 const TAG_REQUEST_NOTIFY_TEMPLATE = "../templates/tag_request_notify.html";
@@ -14,12 +22,19 @@ export class EmailService {
 
     constructor() {
         if (NODE_ENV != "development")
-            this.TRANSPORT = nodemailer.createTransport(MAIL_CONFIG as TransportOptions);
+            this.TRANSPORT = nodemailer.createTransport(
+                MAIL_CONFIG as TransportOptions
+            );
         else
-            this.TRANSPORT = nodemailer.createTransport(MAIL_CONFIG_DEV as TransportOptions);
+            this.TRANSPORT = nodemailer.createTransport(
+                MAIL_CONFIG_DEV as TransportOptions
+            );
     }
 
-    async sendTagRequestNotification(user: AuthUser, tags: string[]): Promise<any> {
+    async sendTagRequestNotification(
+        user: AuthUser,
+        tags: string[]
+    ): Promise<any> {
         let templatePath = path.join(__dirname, TAG_REQUEST_NOTIFY_TEMPLATE);
         let content = fs.readFileSync(templatePath).toString();
         let fullName = `${user.first_name} ${user.last_name}`;
@@ -28,7 +43,12 @@ export class EmailService {
         let tagList = tags.join(", ");
         content = content.replace(/``TAG_LIST``/g, tagList);
 
-        return this.sendEmail(fullName, user.email, "New Asset Tags Request", content);
+        return this.sendEmail(
+            fullName,
+            user.email,
+            "New Asset Tags Request",
+            content
+        );
     }
 
     async sendTagRequestComplete(user: AuthUser, tags: string[]): Promise<any> {
@@ -36,22 +56,38 @@ export class EmailService {
         let content = fs.readFileSync(templatePath).toString();
         let fullName = `${user.first_name} ${user.last_name}`;
 
-        // list tags out 
+        // list tags out
         // this should contain a link to the /my-requested-tags
         let tagList = tags.join(", ");
         content = content.replace(/``TAG_LIST``/g, tagList);
-        content = content.replace(/``TAG_EDIT_URL``/g, `${FRONTEND_URL}/my-requested-tags`);
+        content = content.replace(
+            /``TAG_EDIT_URL``/g,
+            `${FRONTEND_URL}/my-requested-tags`
+        );
 
-        return this.sendEmail(fullName, user.email, "Your Asset Tag Request", content);
+        return this.sendEmail(
+            fullName,
+            user.email,
+            "Your Asset Tag Request",
+            content
+        );
     }
 
-    async sendEmail(toName: string, toEmail: string, subject: string, customContent: string): Promise<any> {
+    async sendEmail(
+        toName: string,
+        toEmail: string,
+        subject: string,
+        customContent: string
+    ): Promise<any> {
         let basePath = path.join(__dirname, BASE_TEMPLATE);
         let baseContent = fs.readFileSync(basePath).toString();
 
         baseContent = baseContent.replace(/``CUSTOM_CONTENT``/, customContent);
         baseContent = baseContent.replace(/``APPLICATION_URL``/g, FRONTEND_URL);
-        baseContent = baseContent.replace(/``APPLICATION_NAME``/g, APPLICATION_NAME);
+        baseContent = baseContent.replace(
+            /``APPLICATION_NAME``/g,
+            APPLICATION_NAME
+        );
         baseContent = baseContent.replace(/``TO_NAME``/g, toName);
         baseContent = baseContent.replace(/``TO_EMAIL``/g, toEmail);
 
@@ -59,11 +95,13 @@ export class EmailService {
             from: MAIL_FROM,
             to: `"${toName}" <${toEmail}>`,
             subject: `${subject} : ${APPLICATION_NAME}`,
-            html: baseContent
+            html: baseContent,
         };
-        
+
         if (toEmail.length == 0) {
-            console.log("Not sending email to " + toName + " without an email address");
+            console.log(
+                "Not sending email to " + toName + " without an email address"
+            );
             return null;
         }
 
