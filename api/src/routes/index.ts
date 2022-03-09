01ae86cb02ebe363bd332ed5060cca830669d5e4
Michael Johnson <michael@icefoganalytics.com>
2022-03-08 17:36:05 -0700

    Removing Max's Asset History

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -6,6 +6,5 @@ export * from './auth';
 export * from './mailcode-router';
 export * from './scan-router';
 export * from './user-router';
-export * from './asset-history-router';
 
 export * as v2Router from './v2';
