b6039d6cef004acb43bfe4f3b75b2ba1cc23b59f
Marlen Brunner <klondikemarlen@gmail.com>
2022-04-01 16:05:47 -0700

    :art: prettify asset-transfer-router.ts

--- api/src/routes/asset-transfer-router.ts
+++ api/src/routes/asset-transfer-router.ts
@@ -1,142 +1,142 @@
-import express, { Request, Response } from "express";
-import { body } from "express-validator";
-import { SortDirection, SortStatement, TransferService } from "../services";
-import { ReturnValidationErrors } from "../middleware";
-import _ from "lodash";
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { SortDirection, SortStatement, TransferService } from '../services';
+import { ReturnValidationErrors } from '../middleware';
+import _ from 'lodash';
 
 export const transferRouter = express.Router();
 const PAGE_SIZE = 10;
 
-import { db, DB_TRUE } from "../data";
+import { db, DB_TRUE } from '../data';
 const transferService = new TransferService(db);
 
 transferRouter.post(
-  "/",
-  [body("page").isInt().default(1), body("itemsPerPage").isInt().default(10)],
-  async (req: Request, res: Response) => {
-    let { query, sortBy, sortDesc, page, itemsPerPage } = req.body;
-    let sort = new Array<SortStatement>();
+	'/',
+	[body('page').isInt().default(1), body('itemsPerPage').isInt().default(10)],
+	async (req: Request, res: Response) => {
+		let { query, sortBy, sortDesc, page, itemsPerPage } = req.body;
+		let sort = new Array<SortStatement>();
 
-    sortBy.forEach((s: string, i: number) => {
-      sort.push({
-        field: s,
-        direction: sortDesc[i]
-          ? SortDirection.ASCENDING
-          : SortDirection.DESCENDING,
-      });
-    });
+		sortBy.forEach((s: string, i: number) => {
+			sort.push({
+				field: s,
+				direction: sortDesc[i]
+					? SortDirection.ASCENDING
+					: SortDirection.DESCENDING,
+			});
+		});
 
-    let skip = (page - 1) * itemsPerPage;
-    let take = itemsPerPage;
+		let skip = (page - 1) * itemsPerPage;
+		let take = itemsPerPage;
 
-    let results = await transferService.doSearch(
-      query,
-      sort,
-      page,
-      itemsPerPage,
-      skip,
-      take
-    );
+		let results = await transferService.doSearch(
+			query,
+			sort,
+			page,
+			itemsPerPage,
+			skip,
+			take
+		);
 
-    return res.json(results);
-  }
+		return res.json(results);
+	}
 );
 
-transferRouter.post("/transfer", async (req: Request, res: Response) => {
-  let { from_owner_id, to_owner_id, rows, action } = req.body;
+transferRouter.post('/transfer', async (req: Request, res: Response) => {
+	let { from_owner_id, to_owner_id, rows, action } = req.body;
 
-  for (let row of rows) {
-    let transfer = {
-      asset_category_id: row.type,
-      request_user: req.user.email,
-      request_date: new Date(),
-      transfer_date: new Date(),
-      condition: row.condition,
-      from_owner_id,
-      to_owner_id,
-      quantity: row.quantity,
-      description: row.tag,
-    };
+	for (let row of rows) {
+		let transfer = {
+			asset_category_id: row.type,
+			request_user: req.user.email,
+			request_date: new Date(),
+			transfer_date: new Date(),
+			condition: row.condition,
+			from_owner_id,
+			to_owner_id,
+			quantity: row.quantity,
+			description: row.tag,
+		};
 
-    await db("asset_transfer").insert(transfer);
-  }
+		await db('asset_transfer').insert(transfer);
+	}
 
-  return res.json({
-    messages: { variant: "success", text: "Transfer saved" },
-  });
+	return res.json({
+		messages: { variant: 'success', text: 'Transfer saved' },
+	});
 });
 
 transferRouter.post(
-  "/transfer-request",
-  async (req: Request, res: Response) => {
-    let { asset, fromOwnerId, rows, condition } = req.body;
-    const default_owner = await db("asset_owner")
-      .where({ default_owner: DB_TRUE })
-      .first();
+	'/transfer-request',
+	async (req: Request, res: Response) => {
+		let { asset, fromOwnerId, rows, condition } = req.body;
+		const default_owner = await db('asset_owner')
+			.where({ default_owner: DB_TRUE })
+			.first();
 
-    if (asset) {
-      let transfer = {
-        asset_item_id: asset.id,
-        request_user: req.user.email,
-        request_date: new Date(),
-        transfer_date: new Date(),
-        condition: `REQUEST: ${condition}`,
-        from_owner_id: fromOwnerId,
-        to_owner_id: default_owner.id,
-        quantity: 1,
-      };
+		if (asset) {
+			let transfer = {
+				asset_item_id: asset.id,
+				request_user: req.user.email,
+				request_date: new Date(),
+				transfer_date: new Date(),
+				condition: `REQUEST: ${condition}`,
+				from_owner_id: fromOwnerId,
+				to_owner_id: default_owner.id,
+				quantity: 1,
+			};
 
-      await db("asset_transfer").insert(transfer);
-    } else {
-      for (let row of rows) {
-        let transfer = {
-          asset_category_id: row.type,
-          request_user: req.user.email,
-          request_date: new Date(),
-          transfer_date: new Date(),
-          condition: `REQUEST: ${row.condition}`,
-          from_owner_id: fromOwnerId,
-          to_owner_id: default_owner.id,
-          quantity: row.quantity,
-        };
+			await db('asset_transfer').insert(transfer);
+		} else {
+			for (let row of rows) {
+				let transfer = {
+					asset_category_id: row.type,
+					request_user: req.user.email,
+					request_date: new Date(),
+					transfer_date: new Date(),
+					condition: `REQUEST: ${row.condition}`,
+					from_owner_id: fromOwnerId,
+					to_owner_id: default_owner.id,
+					quantity: row.quantity,
+				};
 
-        await db("asset_transfer").insert(transfer);
-      }
-    }
+				await db('asset_transfer').insert(transfer);
+			}
+		}
 
-    return res.json({
-      messages: { variant: "success", text: "Transfer saved" },
-    });
-  }
+		return res.json({
+			messages: { variant: 'success', text: 'Transfer saved' },
+		});
+	}
 );
 
-transferRouter.patch("/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
+transferRouter.patch('/:id', async (req: Request, res: Response) => {
+	const { id } = req.params;
 
-  const { asset_category_id, condition, from_owner_id, quantity, to_owner_id } =
-    req.body;
+	const { asset_category_id, condition, from_owner_id, quantity, to_owner_id } =
+		req.body;
 
-  await db("asset_transfer").where({ id }).update({
-    asset_category_id,
-    condition,
-    from_owner_id,
-    quantity,
-    to_owner_id,
-  });
+	await db('asset_transfer').where({ id }).update({
+		asset_category_id,
+		condition,
+		from_owner_id,
+		quantity,
+		to_owner_id,
+	});
 
-  return res.json({
-    data: {},
-    messages: [{ variant: "success", text: "Transfer saved" }],
-  });
+	return res.json({
+		data: {},
+		messages: [{ variant: 'success', text: 'Transfer saved' }],
+	});
 });
 
-transferRouter.delete("/:id", async (req: Request, res: Response) => {
-  const id = parseInt(req.params.id);
+transferRouter.delete('/:id', async (req: Request, res: Response) => {
+	const id = parseInt(req.params.id);
 
-  return transferService.delete(id).then(() =>
-    res.json({
-      data: {},
-      messages: [{ variant: "success", text: "Transfer removed" }],
-    })
-  );
+	return transferService.delete(id).then(() =>
+		res.json({
+			data: {},
+			messages: [{ variant: 'success', text: 'Transfer removed' }],
+		})
+	);
 });
